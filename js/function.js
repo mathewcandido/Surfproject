7564ccf948e589a6ef1bc41c77c92b9db8ce9bf5
Matheus c <matheuscandidoo70@gmail.com>
2021-04-15 14:39:01 -0300

    Colocando o js no Projeto
    Ajustando algumas animações com css e js

---
+++ js/function.js
@@ -0,0 +1,41 @@
+//Animação do texto 
+
+function typeWriter(elemento) {
+
+    var arraynome = elemento.innerHTML.split('');
+
+    elemento.innerHTML = "";
+
+    arraynome.forEach((letra, i) => {
+
+        setTimeout(() => elemento.innerHTML += letra, 25 * i);
+
+
+        console.log(letra)
+    });
+
+}
+const titulo = document.getElementById('p')
+typeWriter(titulo)
+
+
+//modal
+
+
+let imagens = document.querySelectorAll('.small-img');
+let modal = document.querySelector('.modal');
+let modalimg = document.querySelector('#modal-img');
+let btn = document.querySelector('#btn-close');
+let srcVal = "";
+
+for (let i = 0; i < imagens.length; i++) {
+    imagens[i].addEventListener('click', function () {
+        srcVal = imagens[i].getAttribute('src')
+        modalimg.setAttribute('src', srcVal);
+        modal.classList.toggle('modal-active')
+});
+}
+
+btn.addEventListener('click', function () {
+    modal.classList.toggle('modal-active')
+})
